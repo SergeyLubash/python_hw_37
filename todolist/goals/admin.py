0aa4e9daf822e40bc0d65408795b43bba710777a
SergeyLubash <luba-sergei@yandex.ru>
2022-12-29 12:51:21 +0300

    Add todolist/

---
+++ todolist/goals/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import GoalCategory, Board
+
+
+class GoalCategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created", "updated")
+    search_fields = ("title", "user", "board")
+
+
+admin.site.register(GoalCategory, GoalCategoryAdmin)
+admin.site.register(Board)
