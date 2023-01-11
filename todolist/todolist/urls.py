aac76b1d4fb62af85812b9bb35f354e910774243
SergeyLubash <luba-sergei@yandex.ru>
2023-01-11 22:25:42 +0300

    change

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/admin/', admin.site.urls),
-    path('api/core/', include('core.urls')),
-    path('api/oauth/', include('social_django.urls', namespace='social')),
-    path('api/goals/', include('goals.urls')),
-    path('api/bot/', include('bot.urls')),
+    path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
+    path('goals/', include('goals.urls')),
+    path('bot/', include('bot.urls')),
 ]
